2363e5176e274a110d595a1172a1770658dc071a
MarcMcIntosh <marcmcintosh1987@gmail.com>
2020-05-07 20:21:03 +0200

    chore: render content from prismic

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,38 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
+import { RichText } from 'prismic-reactjs';
+
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+
+export const query = graphql`
+  query {
+    prismic {
+      landingPage(uid:"home", lang:"en-gb") {
+        title
+        text
+        image
+      }
+     }
+  }
+`;
+
+const IndexPage = ({ data }) => {
+  console.log({ data });
+  return (
+    <Layout>
+      <SEO title="Home" />
+      {data.prismic.landingPage.title.map(({ text }, index) =>(<h1 key={index}>{text}</h1>))}
+      <RichText render={data.prismic.landingPage.text} />
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
